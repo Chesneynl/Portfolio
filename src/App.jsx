94f73f8ba15eacf4d90d62b692e30d7e9ba88bf7
Chesney Buitendijk <chesneybuitendijk@Chesneys-MBP.domain_not_set.invalid>
2022-11-12 23:05:06 +0100

    move all to one file

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ import Chair from "./components/Chair";
 import * as THREE from "three";
 import Camera from "./components/Camera";
 import React from "react";
+import Scene from "./components/Scene";
 
 const PAGES = 5;
 
@@ -40,81 +41,7 @@ function App() {
 
   return (
     <>
-      <StyledCanvas
-        // frameloop="demand"
-        id="three-canvas-container"
-        shadows
-        dpr={[1, 2]}
-        camera={{ fov: 40 }}
-      >
-        <Suspense fallback={null}>
-          <Scene />
-        </Suspense>
-      </StyledCanvas>
-    </>
-  );
-}
-
-function Scene() {
-  const [focusMesh, setFocusMesh] = useState(false);
-  const [focusAnimationDone, setFocusAnimationDone] = useState(false);
-  const { progress } = useProgress();
-  const { materials, nodes } = useGLTF("/models/Room-glb.gltf");
-  const chairPosition = new THREE.Vector3(-0.91, 0.55, 0.24);
-
-  useFrame(({ camera }) => {
-    if (focusMesh === "chair" && !focusAnimationDone) {
-      camera.position.lerp(new THREE.Vector3(2, 2, 0), 0.1);
-      camera.lookAt(
-        new THREE.Vector3(
-          chairPosition.x - 0.4,
-          chairPosition.y + 0.3,
-          chairPosition.z + 0.5
-        )
-      );
-      camera.updateProjectionMatrix();
-    }
-  });
-
-  return (
-    <>
-      <Lights />
-
-      <Room
-        hide={focusMesh}
-        pages={PAGES}
-        materials={materials}
-        nodes={nodes}
-      />
-      <ScrollControls
-        enabled={!focusMesh}
-        pages={PAGES} // Each page takes 100% of the height of the canvas
-        distance={1} // A factor that increases scroll bar travel (default: 1)
-        damping={5} // Friction, higher is faster (default: 4)
-        infinite={false} // Can also scroll infinitely (default: false)
-      >
-        <Chair
-          color="red"
-          nodes={nodes}
-          isFocused={focusMesh === "chair"}
-          setFocusMesh={setFocusMesh}
-          materials={materials}
-          position={[-0.91, 0.55, 0.24]}
-          pillowColor={
-            new THREE.MeshLambertMaterial({
-              color: "#fae716",
-            })
-          }
-        />
-
-        <Floor />
-
-        {!focusMesh && (
-          <Scroll>
-            <Camera pages={PAGES} materials={materials} nodes={nodes} />
-          </Scroll>
-        )}
-      </ScrollControls>
+      <Scene />
     </>
   );
 }
