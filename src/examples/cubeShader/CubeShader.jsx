71c12c9a64547becdcef28d2be8a53e71d81e1ce
chesneynl <chesneybuitendijk@gmail.com>
2023-01-25 17:07:25 +0100

    fix build

--- src/examples/cubeShader/CubeShader.jsx
+++ src/examples/cubeShader/CubeShader.jsx
@@ -8,6 +8,22 @@ function CubeShader() {
       <Helmet>
         <script
           type="text/javascript"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2666677/three.min.js"
+        ></script>
+        <script
+          type="text/javascript"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2666677/OrbitControls.js"
+        ></script>
+        <script
+          type="text/javascript"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2666677/MyShaderChunks.js"
+        ></script>
+        <script
+          type="text/javascript"
+          src="src/examples/cubeShader/libs/three-pp.js"
+        ></script>
+        <script
+          type="text/javascript"
           src="src/examples/cubeShader/script.js"
         ></script>
       </Helmet>
