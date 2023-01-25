a5a874f5c2caf04847b2e44824bc334fec23aeaa
chesneynl <chesneybuitendijk@gmail.com>
2023-01-25 17:21:22 +0100

    test

--- src/examples/cubeShader/CubeShader.jsx
+++ src/examples/cubeShader/CubeShader.jsx
@@ -18,14 +18,8 @@ function CubeShader() {
           type="text/javascript"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2666677/MyShaderChunks.js"
         ></script>
-        <script
-          type="text/javascript"
-          src="src/examples/cubeShader/libs/three-pp.js"
-        ></script>
-        <script
-          type="text/javascript"
-          src="src/examples/cubeShader/script.js"
-        ></script>
+        <script type="text/javascript" src="./libs/three-pp.js"></script>
+        <script type="text/javascript" src="./script.js"></script>
       </Helmet>
     </>
   );
