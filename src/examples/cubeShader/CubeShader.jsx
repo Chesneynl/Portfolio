3cf1e02b802722ec8a6ae69ce18060748c4b94b6
chesneynl <chesneybuitendijk@gmail.com>
2023-01-25 21:20:56 +0100

    fix cube not showing

--- src/examples/cubeShader/CubeShader.jsx
+++ src/examples/cubeShader/CubeShader.jsx
@@ -18,8 +18,8 @@ function CubeShader() {
           type="text/javascript"
           src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/2666677/MyShaderChunks.js"
         ></script>
-        <script type="text/javascript" src="./libs/three-pp.js"></script>
-        <script type="text/javascript" src="./script.js"></script>
+        <script type="text/javascript" src="../libs/three-pp.js"></script>
+        <script type="text/javascript" src="../libs/script.js"></script>
       </Helmet>
     </>
   );
