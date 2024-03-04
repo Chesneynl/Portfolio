02edf79e79b0f2dbf44e92659eab52cbea99dc07
chesneynl <c.buitendijk@gracious.nl>
2024-03-04 20:57:16 +0100

    working slotmachine

--- src/examples/portfolio/components/sections/Projects.jsx
+++ src/examples/portfolio/components/sections/Projects.jsx
@@ -3,7 +3,14 @@ import SplitType from 'split-type';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 import React, { useEffect, useRef } from 'react';
+
+import VogelsLogo from '../../../../logos/VogelsLogo';
+import MobielLogo from '../../../../logos/MobielLogo';
+import PrazLogo from '../../../../logos/PrazLogo';
+
 gsap.registerPlugin(ScrollTrigger);
+
+let mm = gsap.matchMedia();
 
 export default function Projects() {
     const containerRef = useRef(null);
@@ -16,37 +23,36 @@ export default function Projects() {
         const leftDiv = leftDivRef.current;
 
         let ctx = gsap.context(() => {
-            // Left div animation
-            const splitText = new SplitType(leftDiv, {
-                types: 'words',
-                wordClass: 'large-highlight-word',
-            });
+            mm.add('(min-width: 768px)', () => {
+                const splitText = new SplitType(leftDiv, {
+                    types: 'words',
+                    wordClass: 'large-highlight-word',
+                });
 
-            gsap.from(splitText.words, {
-                yPercent: 100,
-                clipPath: 'inset(0 0 100% 0)', // Starting clip-path
-                stagger: 0.1,
-                scrollTrigger: {
-                    trigger: wrapperRef.current,
-                    start: 'top top',
-                    end: `bottom top`,
-                    pin: containerRef.current,
-                    markers: true,
-                    toggleActions: 'play none none reverse',
-                },
+                gsap.from(splitText.words, {
+                    yPercent: 100,
+                    clipPath: 'inset(0 0 100% 0)', // Starting clip-path
+                    stagger: 0.1,
+                    scrollTrigger: {
+                        trigger: wrapperRef.current,
+                        start: 'top top',
+                        end: `bottom top`,
+                        pin: containerRef.current,
+                        markers: true,
+                        toggleActions: 'play none none reverse',
+                    },
+                });
             });
 
             const listItems = document.querySelectorAll('.project-item');
             // Projects animation
             gsap.to(listItems, {
-                // yPercent: -100,
-                stagger: 0.3,
+                stagger: 0.1,
                 opacity: 1,
                 scrollTrigger: {
                     trigger: wrapperRef.current,
                     start: 'top top',
                     end: `bottom top`,
-
                     markers: true,
                 },
             });
@@ -55,20 +61,101 @@ export default function Projects() {
         return () => ctx.revert();
     }, []);
 
+    const eCommerceWebsites = [
+        {
+            primaryColor: '#594fff',
+            name: 'PRAZ',
+            image: '/images/praz-image.jpeg',
+            logo: <PrazLogo />,
+        },
+        {
+            primaryColor: '#f3dccf',
+            name: 'Mr-fillet',
+            image: '/images/mrfillet-image.jpeg',
+            logo: <VogelsLogo />,
+        },
+        {
+            primaryColor: '#1a2b8f',
+            name: 'Mobiel.nl',
+            image: '/images/mobiel-image.jpeg',
+            logo: <MobielLogo />,
+        },
+        {
+            primaryColor: '#ea5733',
+            name: 'Vogels',
+            image: '/images/vogels-image.png',
+            logo: <VogelsLogo />,
+        },
+    ];
+
     return (
         <div className="w-full" ref={wrapperRef}>
-            <div className="container text-white relative w-full text-8xl overflow-hidden flex" ref={containerRef}>
-                <div className="flex items-center w-full h-screen">
-                    <h2 ref={leftDivRef} className="w-full">
-                        Projects
+            <div
+                className="container text-white relative w-full text-8xl flex flex-col lg:flex-row overflow-hidden"
+                ref={containerRef}
+            >
+                <div className="flex items-center w-full pt-10 lg:h-screen lg:pt-0">
+                    <h2 ref={leftDivRef} className="w-full uppercase">
+                        Work
                     </h2>
                 </div>
-                <div className="w-full gap-4 grid grid-cols-3 pt-36" ref={rightDivRef}>
-                    {Array.from({ length: 20 }, (_, i) => (
-                        <div key={i} className="project-item h-[300px] block bg-green-300 w-full text-sm opacity-0">
-                            Project {i}
+                <div className="w-full relative pt-10 lg:pt-36 flex gap-36 flex-col projects-list" ref={rightDivRef}>
+                    <div className="w-full  lg:absolute top-1/2 trans right-36 h-1/2 lg:-translate-y-1/2">
+                        <div className="w-full text-base text-center mb-5">E-COMMERCE</div>
+                        <div className="w-full gap-4 grid grid-cols-2">
+                            {eCommerceWebsites.map((website, i) => (
+                                <div
+                                    key={`website-${i}`}
+                                    className={`project-item relative  overflow-hidden duration-500 rounded-xl  transition-all filter bg-[${website.primaryColor}] relative h-[300px] lg:h-[250px] uppercase block  w-full text-sm opacity-0 cursor-pointer`}
+                                    style={{ backgroundColor: website.primaryColor }}
+                                >
+                                    <img
+                                        src={website.image}
+                                        alt="Project"
+                                        className="opacity-0 transition-all duration-500"
+                                        style={{
+                                            objectFit: 'cover',
+                                            width: '100%',
+                                            height: '100%',
+                                        }}
+                                    />
+                                    <div className="website-logo absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[120px] transition-all">
+                                        {website.logo}
+                                    </div>
+                                </div>
+                            ))}
                         </div>
-                    ))}
+                    </div>
+                    {/* <div className="w-full gap-4 grid grid-cols-2">
+                        {Array.from({ length: 2 }, (_, i) => (
+                            <div key={i} className="project-item h-[300px] block w-full text-sm opacity-0">
+                                <img
+                                    src="https://via.placeholder.com/300x300"
+                                    alt="Project"
+                                    style={{
+                                        objectFit: 'cover',
+                                        width: '100%',
+                                        height: '100%',
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="w-full gap-4 grid grid-cols-2">
+                        {Array.from({ length: 2 }, (_, i) => (
+                            <div key={i} className="project-item h-[300px] block  w-full text-sm opacity-0">
+                                <img
+                                    src="https://via.placeholder.com/300x300"
+                                    alt="Project"
+                                    style={{
+                                        objectFit: 'cover',
+                                        width: '100%',
+                                        height: '100%',
+                                    }}
+                                />
+                            </div>
+                        ))}
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/examples/portfolio/components/sections/Welcome.jsx
+++ src/examples/portfolio/components/sections/Welcome.jsx
@@ -2,10 +2,13 @@ import SplitType from 'split-type';
 import gsap from 'gsap';
 import React, { useEffect, useRef } from 'react';
 
+let mm = gsap.matchMedia();
+
 export default function Welcome() {
     const nameRef = useRef();
-    const lineRef = useRef();
     const functionRef = useRef();
+    const lineRef = useRef();
+    const functionsRef = useRef();
 
     useEffect(() => {
         if (!nameRef.current) return;
@@ -13,44 +16,150 @@ export default function Welcome() {
         let ctx = gsap.context(() => {
             const tl = gsap.timeline();
 
-            const splitText = new SplitType(nameRef.current, {
-                types: 'words',
-            });
+            // const splitText = new SplitType(nameRef.current, {
+            //     types: 'words',
+            // });
 
-            tl.from(splitText.words, {
-                yPercent: 100,
-                opacity: 0,
-                delay: 1,
-                clipPath: 'inset(0 0 100% 0)', // Starting clip-path
-                stagger: 0.1,
-            });
-
-            tl.to(lineRef.current, {
-                width: '100%',
-                duration: 1,
-                delay: 0.1,
-            });
+            // tl.from(splitText.words, {
+            //     yPercent: 100,
+            //     opacity: 0,
+            //     delay: 0.3,
+            //     clipPath: 'inset(0 0 100% 0)', // Starting clip-path
+            //     stagger: 0.1,
+            // });
 
             tl.to(functionRef.current, {
                 opacity: 1,
-                duration: 1,
-                delay: 0.2,
+                duration: 0.3,
+                delay: 0.1,
+            });
+
+            mm.add('(min-width: 768px)', () => {
+                tl.to(lineRef.current, {
+                    height: '300px',
+                    duration: 0.3,
+                    delay: 0.05,
+                });
+            });
+            mm.add('(max-width: 767px)', () => {
+                tl.to(lineRef.current, {
+                    width: '100%',
+                    duration: 0.3,
+                    delay: 0.05,
+                });
             });
         });
+
+        const words = functionsRef.current.querySelectorAll('.job-title');
+
+        const slotTl = gsap.timeline({ repeat: -1 });
+
+        slotTl
+            .to(functionsRef.current, {
+                duration: 0.5,
+                delay: 1,
+                y: '-=40', // Move all words up
+                ease: 'none',
+            })
+            .to(
+                words[1],
+                {
+                    opacity: 0.1,
+                    duration: 0.5,
+                    ease: 'none',
+                },
+                '-=0.5',
+            )
+            .to(
+                words[2],
+                {
+                    opacity: 1,
+                    duration: 0.5,
+                    ease: 'none',
+                },
+                '-=0.5',
+            )
+            .to(functionsRef.current, {
+                duration: 0.5,
+                y: '-=40', // Move all words up
+                ease: 'none',
+                delay: 1.5,
+            })
+            .to(
+                words[2],
+                {
+                    opacity: 0.1,
+                    duration: 0.5,
+                    ease: 'none',
+                },
+                '-=0.5',
+            )
+            .to(
+                words[3],
+                {
+                    opacity: 1,
+                    duration: 0.5,
+                    ease: 'none',
+                },
+                '-=0.5',
+            )
+            .to(functionsRef.current, {
+                duration: 0.5,
+                y: '-=40', // Move all words up
+                ease: 'none',
+                delay: 1,
+            })
+            .to(
+                words[3],
+                {
+                    opacity: 0.1,
+                    duration: 0.5,
+                    ease: 'none',
+                },
+                '-=0.5',
+            )
+            .to(
+                words[4],
+                {
+                    opacity: 1,
+                    duration: 0.5,
+                    ease: 'none',
+                },
+                '-=0.5',
+            )
+            .to(functionsRef.current, {
+                duration: 0,
+                y: '0', // Move all words up
+                ease: 'none',
+                delay: 1,
+            });
 
         return () => ctx.revert();
     }, [nameRef?.current]);
 
     return (
-        <div className="container text-white flex flex-col md:flex-row-reverse h-screen w-full justify-center gap-4 md:items-center">
-            <h1 ref={nameRef} className="text-7xl md:text-8xl pb-3 font-micro uppercase">
+        <div className="container text-white flex flex-col-reverse lg:flex-row-reverse h-screen w-full justify-center gap-4 lg:gap-8 lg:items-center">
+            <h1
+                ref={nameRef}
+                className="text-3xl lg:text-4xl pb-3 font-micro uppercase lg:pb-0 text-[#8d49f7] flex items-center justify-center gap-4"
+            >
+                <div className="h-[120px] overflow-hidden">
+                    <div className="whitespace-nowrap" ref={functionsRef}>
+                        <div className="job-title opacity-10">Front-end</div>
+                        <div className="job-title opacity-1">Creative</div>
+                        <div className="job-title opacity-10">Freelance</div>
+                        <div className="job-title opacity-10">Front-end</div>
+                        <div className="job-title opacity-10">Creative</div>
+                        <div className="job-title opacity-10">Freelance</div>
+                    </div>
+                </div>
+                <div>developer</div>
+            </h1>
+            <div ref={lineRef} className="w-0 h-1 rounded-full bg-white lg:h-0 flex-grow-0 lg:w-1 flex-shrink-0" />
+            <h2 ref={functionRef} className="text-7xl lg:text-6xl opacity-0 pt-1 uppercase lg:text-right lg:pt-0">
                 Chesney
                 <br />
                 Buitendijk
-            </h1>
-            <div ref={lineRef} className="w-0 h-1 rounded-full bg-white md:h-full flex-grow-0" />
-            <h2 ref={functionRef} className="text-3xl md:text-8xl opacity-0 pt-1">
-                Freelance developer
             </h2>
         </div>
     );
