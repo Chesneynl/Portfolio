43bd5380d3ef132d1fc15c26c38f7ec0d2c4bd9d
chesneynl <c.buitendijk@gracious.nl>
2024-05-17 20:56:18 +0200

    remove localhost

--- src/examples/portfolio/components/sections/Connect.jsx
+++ src/examples/portfolio/components/sections/Connect.jsx
@@ -108,7 +108,7 @@ export default function Connect() {
         setIsSubmitting(true);
 
         try {
-            const data = await fetch('http://localhost:5000/send-email', {
+            const data = await fetch('/send-email', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
